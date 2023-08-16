81478044f676ca7c0f44c80751b506c58e11c830
odonald <eugeneodonald@gmail.com>
2023-08-17 00:41:47 +0200

    Update Language import: Included Import button in Languagdialog Included common languages prepopulated

--- main.py
+++ main.py
@@ -114,51 +114,71 @@ def open_url_in_browser(local_ip, port_number):
     url = f"http://{local_ip}:{port_number}"
     webbrowser.open(url)
 
+COMMON_LANGUAGES = ['Arabic', 'Bengali', 'Chinese', 'English', 'French', 'German', 'Hindi', 'Indonesian', 'Italian', 'Japanese', 'Javanese', 'Korean', 'Malay', 'Marathi', 'Portuguese', 'Punjabi', 'Russian', 'Spanish', 'Tamil', 'Telugu', 'Turkish', 'Ukrainian', 'Urdu', 'Vietnamese', 'Wu', 'Xhosa', 'Yoruba', 'Zulu', 'Cantonese', 'Farsi', 'Filipino', 'Gujarati', 'Hausa', 'Haitian Creole', 'Igbo', 'Kannada', 'Maithili', 'Odia', 'Romanian', 'Thai']
 
 class LanguageDialog(tk.simpledialog.Dialog):
     def body(self, master):
         sorted_languages = dict(sorted({**available_languages, **additional_languages}.items(), key=lambda item: item[0]))
         
+        # Label for the search bar
+        label = tk.Label(master, text="Search for a language:")
+        label.grid(row=0, column=0, sticky="w", padx=5, pady=5)
+        
         # Create a search bar with autofill functionality
         self.search_var = tk.StringVar()
         self.search_var.trace("w", self.update_list)
         self.entry = tk.Entry(master, textvariable=self.search_var, width=25)
-        self.entry.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        self.entry.grid(row=1, column=0, padx=5, pady=5, sticky="w")
         
         # Create the language listbox
-        self.language_listbox = tk.Listbox(master, selectmode="single", exportselection=False)
+        self.language_listbox = tk.Listbox(master, selectmode="single", exportselection=False, height=10)
         for language in sorted_languages.keys():
             self.language_listbox.insert(tk.END, language)
-        self.language_listbox.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+        self.language_listbox.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
         self.language_listbox.bind("<<ListboxSelect>>", self.on_select)
         
-        # Change the font color of the listbox to white
-        # self.language_listbox.configure(fg="white")
+        # Add a scrollbar to the listbox
+        scrollbar = ttk.Scrollbar(master, orient="vertical", command=self.language_listbox.yview)
+        scrollbar.grid(row=2, column=1, sticky="ns")
+        self.language_listbox.configure(yscrollcommand=scrollbar.set)
         
         self.update_list()
         
         return self.entry
 
+    
     def update_list(self, *args):
         search_term = self.search_var.get().lower()
         self.language_listbox.delete(0, tk.END)
         items = []
-        if len(search_term) >= 3:
+        if len(search_term) < 3:
+            items = COMMON_LANGUAGES
+        else:
             for language in {**available_languages, **additional_languages}.keys():
                 if search_term in language.lower():
                     items.append(language)
             items = sorted(items, key=lambda x: (len(x), x.startswith(search_term), x))
+            
         for item in items:
             self.language_listbox.insert(tk.END, item)
 
     def on_select(self, event):
-        selected_language = self.language_listbox.get(self.language_listbox.curselection())
-        language_code = {**available_languages, **additional_languages}[selected_language]
-        import_additional_translation(selected_language, language_code)
+        self.selected_language = self.language_listbox.get(self.language_listbox.curselection())
 
     def apply(self):
-        pass
+        language_code = {**available_languages, **additional_languages}[self.selected_language]
+        import_additional_translation(self.selected_language, language_code)
+        # You can add a message here for feedback to the user about the successful import
 
+    def buttonbox(self):
+        box = tk.Frame(self)
+        w = ttk.Button(box, text="Import", width=10, command=self.ok, default=tk.ACTIVE)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+        w = ttk.Button(box, text="Cancel", width=10, command=self.cancel)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+        box.pack()
 def import_additional_language():
     global language_switcher_values
     LanguageDialog(root)
@@ -325,7 +345,7 @@ def update_label():
         # prev_line_label.unbind("<Button-1>")
         # prev_line_label.bind("<Button-1>", lambda event, line=prev_line: set_current_line(line))
 
-        current_line_label.configure(text=f"Line {current_line + 1}:\n{current_lang_line}")
+        current_line_label.configure(text=f"Current Line:\n{current_lang_line}")
         current_line_label.unbind("<Button-1>")
         current_line_label.bind("<Button-1>", lambda event, line=current_line: set_current_line(line))
 
