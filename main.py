c5799c5ce55f47498e45f98dffb5cfc167dca021
Lugina <lugina.oezdemir@code.berlin>
2024-03-17 19:31:56 +0100

    refactor: Moved system setup logic from main.py to setup.py

--- main.py
+++ main.py
@@ -109,30 +109,3 @@ root.protocol("WM_DELETE_WINDOW", close_program)
 #         update_server_indicator(False)
 
 
-# Get the directory where the application is running
-if getattr(sys, 'frozen', False):
-    application_path = os.path.dirname(sys.executable)
-elif __file__:
-    application_path = os.path.dirname(__file__)
-
-# Check the type of operating system
-if os.name == 'nt':  # For Windows
-    logs_folder = 'logs'
-else:  # For Unix and MacOS
-    logs_folder = '.logs'  # Use a dot to make the folder hidden on Unix systems
-
-log_dir = os.path.join(application_path, logs_folder)
-
-# Ensure the directory exists, if not, create it
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-stdout_log_path = os.path.join(log_dir, 'my_stdout.log')
-stderr_log_path = os.path.join(log_dir, 'my_stderr.log')
-
-sys.stdout = open(stdout_log_path, 'w')
-sys.stderr = open(stderr_log_path, 'w')
-
-root.bind("<KeyPress>", on_key_press)
-# Run the Tkinter root
-root.mainloop()
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import os
+import sys
 
 APP = ['main.py']
 APP_NAME = "Opera Access"
@@ -19,3 +21,32 @@ setup(
     packages=find_packages(),
 
 )
+
+
+# Get the directory where the application is running
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
+
+# Check the type of operating system
+if os.name == 'nt':  # For Windows
+    logs_folder = 'logs'
+else:  # For Unix and MacOS
+    logs_folder = '.logs'  # Use a dot to make the folder hidden on Unix systems
+
+log_dir = os.path.join(application_path, logs_folder)
+
+# Ensure the directory exists, if not, create it
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+stdout_log_path = os.path.join(log_dir, 'my_stdout.log')
+stderr_log_path = os.path.join(log_dir, 'my_stderr.log')
+
+sys.stdout = open(stdout_log_path, 'w')
+sys.stderr = open(stderr_log_path, 'w')
+
+root.bind("<KeyPress>", on_key_press)
+# Run the Tkinter root
+root.mainloop()
