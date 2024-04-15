00e43e3b37429356bd9890479147bf232dd8cda6
odonald <eugeneodonald@gmail.com>
2024-04-15 11:31:56 +0200

    Security Fix: switched off debug mode in server

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ class AppConfig:
     APPEARANCE_MODE = "System"
     APP_TITLE = "Opera Access 1.0"
     WINDOW_SIZE = "1000x550"
-    DEBUG = True
+    DEBUG = False
     HOST = socket.gethostbyname(socket.gethostname())
     PORT = 7832
     URL = f"http://{HOST}:{PORT}/stream/push"
