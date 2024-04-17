a049e037e5290f64dc94a00b1b5a6ae030c8b91c
odonald <eugeneodonald@gmail.com>
2024-04-17 23:12:51 +0200

    Refactor: moved logging configuration to new file -> utils/logger_config.py | Cleared unused code from main.py | Renamed main.py -> run.py as single application point of entry

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-from main import main  # Import the main function directly
+from logic.application import Application
+from ui.ui import create_main_window
+from logic.application import Application
+
+def main():
+    root = create_main_window()
+    Application(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
