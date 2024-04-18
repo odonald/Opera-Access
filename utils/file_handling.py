31e1d90d2f404be842ab295c941d5220313412c0
odonald <eugeneodonald@gmail.com>
2024-04-18 20:16:52 +0200

    Refactoring reset: Moved server code back from server.py into application.py

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -13,45 +13,47 @@ from ui.file_menu import FileMenu
 class QrCode:
     def __init__(self, root):
         self.root = root
-        self.ui = UserInterface(root) 
-        self.bind_show_qr_button()
+        self.ui = UserInterface(root)
+        self.ui = UserInterface(root)
+        self.bind_show_qr_button(root)
 
     local_ip = AppConfig.HOST
     port_number = AppConfig.PORT
 
-    def bind_show_qr_button(self):
-        self.ui.show_qr_button.configure(command=lambda: self.show_qr_code())          
+    def bind_show_qr_button(self, root):
+        self.ui.show_qr_button.configure(command=lambda: self.show_qr_code(root))
 
-    def show_qr_code(self):
-            url = f"http://{self.local_ip}:{self.port_number}"
+    def show_qr_code(self, root):
+        url = f"http://{self.local_ip}:{self.port_number}"
 
-            def on_qr_click(event):
+        def on_qr_click(event):
                 self.save_qr_code()
 
-            global qr_img
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=20,
-                border=4,
-            )
-            qr.add_data(url)
-            qr.make(fit=True)
+                global qr_img
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=20,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
 
-            img = qr.make_image(fill_color="black", back_color="white")
-            img_tk = ImageTk.PhotoImage(img)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_tk = ImageTk.PhotoImage(img)
 
-            qr_img = img
+        qr_img = img
 
-            qr_window = tk.Toplevel()
-            qr_window.title("QR Code")
-            qr_window.geometry("600x600")
+        qr_window = tk.Toplevel(root)
+        qr_window = tk.Toplevel()
+        qr_window.title("QR Code")
+        qr_window.geometry("600x600")
 
-            qr_label = tk.Label(qr_window, image=img_tk)
-            qr_label.image = img_tk
-            qr_label.pack()
+        qr_label = tk.Label(qr_window, image=img_tk)
+        qr_label.image = img_tk
+        qr_label.pack()
 
-            qr_label.bind("<Button-1>", on_qr_click)
+        qr_label.bind("<Button-1>", on_qr_click)
 
-            qr_window.mainloop()
-            
+        qr_window.mainloop()
+        
