ca45f1d5cead279178f3c9dcf551c5371909b020
odonald <eugeneodonald@gmail.com>
2024-04-19 18:50:06 +0200

    Testing: added send_to_server unittest

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,10 +1,15 @@
 import unittest
-from unittest import mock
+from unittest.mock import patch
 from logic.application import Application
 
 class TestApplication(unittest.TestCase):
     def setUp(self):
         self.app = Application(None)
+        self.app.sse_url = "http://example.com/sse"
+        self.app.additional_languages = {
+            "en": ["Line 1", "Line 2", "Line 3"],
+            "es": ["Línea 1", "Línea 2", "Línea 3"]
+        }
 
     def test_combine_files_both_files_not_empty(self):
         original_lines = ["line 1", "line 2", "line 3"]
@@ -55,3 +60,41 @@ class TestApplication(unittest.TestCase):
         expected_combined_lines = [("line 1", "translation 1"), ("", "translation 2"), ("line 3", "")]
 
         self.assertEqual(combined_lines, expected_combined_lines)
+
+
+    @patch("requests.post")
+    def test_send_to_server(self, mock_post):
+        line_number = 2
+
+        self.app.send_to_server(line_number)
+
+        expected_message = {
+            "type": "message",
+            "content": {
+                "English": "Line 3",
+                "Spanish": "Línea 3"
+            }
+        }
+        mock_post.assert_called_once_with(self.app.sse_url, json=expected_message)
+
+    @patch("requests.post")
+    def test_send_to_server_non_existent_language_code(self, mock_post):
+        self.app.additional_languages = {
+            "en": ["Line 1", "Line 2", "Line 3"],
+            "invalid": ["Invalid 1", "Invalid 2", "Invalid 3"]
+        }
+        line_number = 1
+
+        with self.assertRaises(KeyError):
+            self.app.send_to_server(line_number)
+
+        mock_post.assert_not_called()
+
+    @patch("requests.post")
+    def test_send_to_server_non_integer_line_number(self, mock_post):
+        line_number = "Not and Integer"
+
+        with self.assertRaises(TypeError):
+            self.app.send_to_server(line_number)
+
+        mock_post.assert_not_called()
