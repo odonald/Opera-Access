259d4570438b9c3d2b5ac9bc1ced5d9252e5f856
odonald <eugeneodonald@gmail.com>
2024-04-21 23:16:07 +0200

    UI-Layouting fix

--- ui/ui.py
+++ ui/ui.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 class UserInterface:
     def __init__(self, root):
         self.root = root
-        self.navigation_frame, self.navigation_frame2 = self.main_frame()
+        self.navigation_frame, self.navigation_frame2, self.navigation_frame3 = self.main_frame()
         self.sidebar_frame = self.create_sidebar_frame()
         self.canvas = self.create_canvas()
         self.inner_frame = self.create_inner_frame()
@@ -59,30 +59,32 @@ class UserInterface:
         return next_line_button
 
     def create_line_number_entry(self):
-        line_number_entry = ctk.CTkEntry(self.navigation_frame2, placeholder_text="Jump to line")
+        line_number_entry = ctk.CTkEntry(self.navigation_frame3, placeholder_text="Jump to line")
         line_number_entry.grid(row=2, column=1, padx=10, pady=10, sticky="e")
         return line_number_entry
 
     def create_go_button(self, jump_to_line_command):
-        go_button = ctk.CTkButton(self.navigation_frame2, fg_color="transparent", text_color=("gray10", "#DCE4EE"), border_width=2, text="Go", command=jump_to_line_command)
+        go_button = ctk.CTkButton(self.navigation_frame3, fg_color="transparent", text_color=("gray10", "#DCE4EE"), border_width=2, text="Go", command=jump_to_line_command)
         go_button.grid(row=2, column=2, padx=10, pady=10, sticky="w")
         return go_button
 
     def create_progress_bar(self):
         progress = ctk.CTkProgressBar(self.root, orientation="horizontal")
-        progress.grid(row=0, column=0, columnspan=4, padx=0, pady=0, sticky="new")
+        progress.grid(row=0, column=1, columnspan=4, padx=0, pady=0, sticky="new")
         progress.configure(mode="determinate")
         progress.set(0)
         return progress
     
     def main_frame(self):
         navigation_frame = tk.LabelFrame(self.root, height=900)
-        navigation_frame.grid(row=1, rowspan=6, column=1, columnspan=3, padx=20, pady=10, sticky="nesw")
+        navigation_frame.grid(row=1, rowspan=6, column=1, columnspan=4, padx=20, pady=10, sticky="nesw")
         navigation_frame.grid_rowconfigure(0, weight=1)
         navigation_frame.grid_columnconfigure(0, weight=1)
         navigation_frame2 = ctk.CTkFrame(self.root,fg_color="transparent", width=500, height=200, corner_radius=4, border_width=0)
-        navigation_frame2.grid(row=8, column=1,rowspan=3, padx=0, pady=0, sticky="e")
-        return navigation_frame, navigation_frame2
+        navigation_frame2.grid(row=8, column=2,rowspan=1, padx=0, pady=0, sticky="w")
+        navigation_frame3 = ctk.CTkFrame(self.root,fg_color="transparent", width=500, height=200, corner_radius=4, border_width=0)
+        navigation_frame3.grid(row=8, column=1,rowspan=1, padx=0, pady=0, sticky="e")
+        return navigation_frame, navigation_frame2, navigation_frame3
     
     def create_canvas(self):
         canvas = tk.Canvas(self.navigation_frame)
@@ -91,8 +93,8 @@ class UserInterface:
     
     def create_inner_frame(self):
         inner_frame = ctk.CTkFrame(self.canvas)
-        inner_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
-        inner_frame.grid_columnconfigure(1, weight=1)
+        inner_frame.grid_rowconfigure((0, 1, 2), weight=1)
+        inner_frame.grid_columnconfigure(0, weight=1)
         return inner_frame
     
     def create_canvas_frame(self):
@@ -101,9 +103,9 @@ class UserInterface:
 
     def create_sidebar_frame(self):
         sidebar_frame = ctk.CTkFrame(self.root, width=100, corner_radius=0, border_width=2)
-        sidebar_frame.grid(row=0, column=0,rowspan=10, sticky="nsw")
-        sidebar_frame.grid_rowconfigure(10, weight=0)
-        sidebar_frame.grid_columnconfigure(0, weight=1)
+        sidebar_frame.grid(row=0, column=0, rowspan=10, sticky="nsew")
+        sidebar_frame.grid_rowconfigure(8, weight=1)
+        sidebar_frame.grid_columnconfigure(1, weight=1)
         return sidebar_frame
         
     def button_go_to_website(self):
@@ -123,13 +125,13 @@ class UserInterface:
     
     def labels_inner_frame(self):
         current_line_label = ctk.CTkLabel(self.inner_frame, text_color=("Yellow", "#FFD90F"), text="Please import a language or load a session.\n +\n <--- Choose display language", font=("", 25))
-        current_line_label.grid(row=5, column=0, padx=10, pady=10)
+        current_line_label.grid(row=5, column=1, padx=10, pady=10)
         prev_line_labels = [ctk.CTkLabel(self.inner_frame, wraplength=400, text="---") for _ in range(5)]
         for index, label in enumerate(prev_line_labels, start=0):
-            label.grid(row=index, column=0, padx=10, pady=10)
+            label.grid(row=index, column=1, padx=10, pady=10)
         next_line_labels = [ctk.CTkLabel(self.inner_frame, wraplength=400, text="---") for _ in range(20)]
         for index, label in enumerate(next_line_labels, start=6):
-                label.grid(row=index, column=0, padx=10, pady=10)
+                label.grid(row=index, column=1, padx=10, pady=10)
         return current_line_label, prev_line_labels, next_line_labels
 
 class Events:
@@ -143,6 +145,6 @@ def create_main_window():
     root.configure(bg=ctk.set_appearance_mode("System"))
     root.geometry(f"{1000}x{550}")
     root.grid_rowconfigure((0,1,2,3,4,5,6,7,8,9), weight=1)
-    root.columnconfigure((0,1,2,3,4), weight=1)
+    root.columnconfigure((0,1,2), weight=1)
 
     return root
