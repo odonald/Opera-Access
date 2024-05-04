ac99aae6c3417dfca56516aa887f528ee6f6aff5
odonald <eugeneodonald@gmail.com>
2024-05-04 11:47:06 +0200

    Docstrings: included docstrings for logger_config.py

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -5,6 +5,12 @@ import argparse
 
 
 def parse_arguments():
+    """
+    Parse command line arguments.
+
+    Returns:
+        bool: True if debug mode is enabled, False otherwise.
+    """
     parser = argparse.ArgumentParser(description='My Application')
     parser.add_argument('--debug', action='store_true',
                         help='Enable debug mode')
@@ -13,6 +19,22 @@ def parse_arguments():
 
 
 def setup_logging(debug_mode):
+    """
+    Set up logging configuration based on the debug mode.
+
+    Parameters:
+        debug_mode (bool): True if debug mode is enabled, False otherwise.
+
+    The function sets up the logging configuration based on the debug mode. If debug mode is enabled, the logging level is set to DEBUG and the log messages will be displayed on the terminal. If debug mode is disabled, the log messages will be written to a log file named 'app_debug.log'.
+
+    If debug mode is disabled, the function also creates a log directory based on the operating system. On Windows, the log directory is named 'logs', and on other operating systems, it is named '.logs'. The log directory is created if it does not exist.
+
+    The function redirects the standard output and standard error to separate log files named 'my_stdout.log' and 'my_stderr.log' respectively, located in the log directory.
+
+    Example usage:
+        debug_mode = parse_arguments()
+        setup_logging(debug_mode)
+    """
     if debug_mode:
         logging.basicConfig(level=logging.DEBUG)
     else:
